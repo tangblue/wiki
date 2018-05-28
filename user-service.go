01ea913e7ec165107d9278722af4d36927e8b458
phoofeng <phoofeng@gmail.com>
2018-05-28 23:37:59 +0900

    Add authentication.

--- user-service.go
+++ user-service.go
@@ -8,13 +8,90 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
-	restful "github.com/emicklei/go-restful"
-	restfulspec "github.com/emicklei/go-restful-openapi"
 	"github.com/go-openapi/spec"
+	"github.com/tangblue/goapi/restful"
+	"github.com/tangblue/goapi/restfulspec"
 )
 
+type LoginInfo struct {
+	Name     string `json:"name" description:"user name"`
+	Password string `json:"password" description:"password"`
+}
+
 type JWTToken struct {
-	Token string `json:"token"`
+	Token string `json:"token" description:"JWT token"`
+}
+
+type Auth struct {
+	secret          string
+	hpAuthorization *restful.Parameter
+}
+
+func (a *Auth) WebService() *restful.WebService {
+	ws := new(restful.WebService)
+	ws.Path("/login").
+		Consumes(restful.MIME_JSON).
+		Produces(restful.MIME_JSON)
+
+	ws.Route(ws.POST("").To(a.createToken).
+		Doc("login").
+		Metadata(restfulspec.KeyOpenAPITags, []string{"authentication"}).
+		Reads(LoginInfo{}).
+		Returns(http.StatusOK, "OK", JWTToken{}).
+		Returns(http.StatusInternalServerError, "Internal Server Error", nil).
+		Returns(http.StatusUnprocessableEntity, "Bad user name or password", nil))
+
+	return ws
+}
+
+func (a *Auth) basicAuthenticate(req *restful.Request, resp *restful.Response, next func(*restful.Request, *restful.Response)) {
+	// usr/pwd = admin/admin
+	u, p, ok := req.Request.BasicAuth()
+	if !ok || u != "admin" || p != "admin" {
+		resp.AddHeader("WWW-Authenticate", "Basic realm=Protected Area")
+		resp.WriteErrorString(http.StatusUnauthorized, "401: Not Authorized")
+		return
+	}
+	next(req, resp)
+}
+
+func (a *Auth) createToken(req *restful.Request, resp *restful.Response) {
+	li := LoginInfo{}
+	if err := req.ReadEntity(&li); err != nil {
+		resp.WriteError(http.StatusInternalServerError, err)
+		return
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": li.Name,
+	})
+	tokenString, err := token.SignedString([]byte(a.secret))
+	if err != nil {
+		resp.WriteError(http.StatusInternalServerError, err)
+	}
+	resp.WriteEntity(JWTToken{Token: tokenString})
+}
+
+func (a *Auth) JWTAuthenticate(req *restful.Request, resp *restful.Response, next func(*restful.Request, *restful.Response)) {
+	ah := req.GetParameter(a.hpAuthorization)
+	bt := strings.Fields(ah)
+	if len(bt) != 2 || !strings.EqualFold(bt[0], "bearer") {
+		resp.WriteErrorString(http.StatusUnauthorized, "401: Not Authorized")
+		return
+	}
+
+	token, err := jwt.Parse(bt[1], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return []byte(a.secret), nil
+	})
+	if err != nil || !token.Valid {
+		resp.WriteErrorString(http.StatusUnauthorized, "401: Not Authorized")
+		return
+	}
+	log.Printf("Claims: %v", token.Claims)
+
+	next(req, resp)
 }
 
 type User struct {
@@ -26,48 +103,21 @@ type User struct {
 type UserResource struct {
 	// normally one would use DAO (data access object)
 	users map[int]User
-}
+	ppUID *restful.Parameter
 
-func basicAuthenticate(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	// usr/pwd = admin/admin
-	u, p, ok := req.Request.BasicAuth()
-	if !ok || u != "admin" || p != "admin" {
-		resp.AddHeader("WWW-Authenticate", "Basic realm=Protected Area")
-		resp.WriteErrorString(401, "401: Not Authorized")
-		return
-	}
-	chain.ProcessFilter(req, resp)
-}
-
-const secret = "secret"
-
-func JWTAuthenticate(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	ah := req.HeaderParameter("authorization")
-	bearerToken := strings.Fields(ah)
-	if len(bearerToken) != 2 {
-		resp.WriteErrorString(401, "401: Not Authorized")
-		return
-	}
-
-	token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("There was an error")
-		}
-		return []byte(secret), nil
-	})
-	if err != nil || !token.Valid {
-		resp.WriteErrorString(401, "401: Not Authorized")
-		return
-	}
-	log.Printf("Claims: %v", token.Claims)
-
-	chain.ProcessFilter(req, resp)
+	auth *Auth
 }
 
 func (u UserResource) WebService() *restful.WebService {
-	printPath := func(request *restful.Request, response *restful.Response, chain *restful.FilterChain) {
-		log.Printf("Path: %v", request.Request.URL.Path)
-		chain.ProcessFilter(request, response)
+	printPath := func(req *restful.Request, resp *restful.Response, next func(*restful.Request, *restful.Response)) {
+		log.Printf("Path: %v", req.Request.URL.Path)
+		next(req, resp)
+	}
+	tagUsers := func(b *restful.RouteBuilder) {
+		b.Metadata(restfulspec.KeyOpenAPITags, []string{"users"})
+	}
+	addAuth := func(b *restful.RouteBuilder) {
+		b.Filter(u.auth.JWTAuthenticate).Param(u.auth.hpAuthorization)
 	}
 
 	ws := new(restful.WebService)
@@ -76,132 +126,121 @@ func (u UserResource) WebService() *restful.WebService {
 		Produces(restful.MIME_JSON, restful.MIME_XML).
 		Filter(printPath)
 
-	tags := []string{"users"}
-	tagUsers := func(b *restful.RouteBuilder) {
-		b.Metadata(restfulspec.KeyOpenAPITags, tags)
-	}
-
-	uid := ws.PathParameter("userID", "identifier of the user").DataType("integer").DefaultValue("1")
 	ws.Route(ws.GET("/").To(u.findAllUsers).
 		Doc("get all users").
 		Do(tagUsers).
-		Filter(JWTAuthenticate).
-		Writes([]User{}).
-		Returns(200, "OK", []User{}))
-
-	ws.Route(ws.GET("/authenticate").To(u.createToken).
-		Doc("authenticate").
-		Do(tagUsers).
-		Filter(basicAuthenticate).
-		Returns(200, "OK", JWTToken{}))
-
-	ws.Route(ws.GET("/{userID}").To(u.findUser).
-		Doc("get a user").
-		Do(tagUsers).
-		Param(uid).
-		Writes(User{}).
-		Returns(200, "OK", User{}).
-		Returns(404, "Not Found", nil))
-
-	ws.Route(ws.PUT("/{userID}").To(u.updateUser).
-		Doc("update a user").
-		Do(tagUsers).
-		Param(uid).
-		Reads(User{}).
-		Writes(User{}).
-		Returns(200, "OK", User{}).
-		Returns(404, "Not Found", nil))
+		Filter(u.auth.basicAuthenticate).
+		Returns(http.StatusOK, "OK", []User{}).
+		Returns(http.StatusUnauthorized, "Not Authorized", nil))
 
 	ws.Route(ws.PUT("").To(u.createUser).
 		Doc("create a user").
-		Do(tagUsers).
-		Reads(User{}))
+		Do(tagUsers, addAuth).
+		Reads(User{}).
+		Returns(http.StatusCreated, "Created", User{}).
+		Returns(http.StatusUnauthorized, "Not Authorized", nil))
 
-	ws.Route(ws.DELETE("/{userID}").To(u.removeUser).
-		Doc("delete a user").
+	ws.Route(ws.GET("/{%s}", u.ppUID).To(u.findUser).
+		Doc("get a user").
 		Do(tagUsers).
-		Param(uid))
+		Returns(http.StatusOK, "OK", User{}).
+		Returns(http.StatusNotFound, "Not Found", nil))
+
+	ws.Route(ws.PUT("/{%s}", u.ppUID).To(u.updateUser).
+		Doc("update a user").
+		Do(tagUsers, addAuth).
+		Reads(User{}).
+		Returns(http.StatusOK, "OK", User{}).
+		Returns(http.StatusUnauthorized, "Not Authorized", nil).
+		Returns(http.StatusNotFound, "Not Found", nil))
+
+	ws.Route(ws.DELETE("/{%s}", u.ppUID).To(u.removeUser).
+		Doc("delete a user").
+		Do(tagUsers, addAuth).
+		Returns(http.StatusNoContent, "No Content", nil).
+		Returns(http.StatusUnauthorized, "Not Authorized", nil).
+		Returns(http.StatusNotFound, "Not Found", nil))
 
 	return ws
 }
 
-func (u UserResource) createToken(request *restful.Request, response *restful.Response) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": "admin",
-	})
-	tokenString, err := token.SignedString([]byte(secret))
-	if err != nil {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
-	}
-	response.WriteEntity(JWTToken{Token: tokenString})
-}
-
-func (u UserResource) findAllUsers(request *restful.Request, response *restful.Response) {
+func (u UserResource) findAllUsers(req *restful.Request, resp *restful.Response) {
 	list := []User{}
 	for _, each := range u.users {
 		list = append(list, each)
 	}
-	response.WriteEntity(list)
+	resp.WriteEntity(list)
 }
 
-func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
-	id, err := strconv.Atoi(request.PathParameter("userID"))
+func (u UserResource) findUser(req *restful.Request, resp *restful.Response) {
+	id, err := strconv.Atoi(req.GetParameter(u.ppUID))
 	if err != nil {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+		resp.WriteErrorString(http.StatusBadRequest, "User ID is invalid.")
 		return
 	}
 
 	if usr, ok := u.users[id]; !ok {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+		resp.WriteErrorString(http.StatusNotFound, "User could not be found.")
 	} else {
-		response.WriteEntity(usr)
+		resp.WriteEntity(usr)
 	}
 }
 
-func (u *UserResource) updateUser(request *restful.Request, response *restful.Response) {
-	id, err := strconv.Atoi(request.PathParameter("userID"))
+func (u *UserResource) updateUser(req *restful.Request, resp *restful.Response) {
+	id, err := strconv.Atoi(req.GetParameter(u.ppUID))
 	if err != nil {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+		resp.WriteErrorString(http.StatusBadRequest, "User ID is invalid.")
 		return
 	}
 
 	usr, ok := u.users[id]
 	if !ok {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+		resp.WriteErrorString(http.StatusNotFound, "User could not be found.")
 		return
 	}
 
-	if err := request.ReadEntity(&usr); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
+	if err := req.ReadEntity(&usr); err != nil {
+		resp.WriteError(http.StatusInternalServerError, err)
 		return
 	}
 
 	usr.ID = id
 	u.users[id] = usr
-	response.WriteEntity(usr)
+	resp.WriteEntity(usr)
 }
 
-func (u *UserResource) createUser(request *restful.Request, response *restful.Response) {
+func (u *UserResource) createUser(req *restful.Request, resp *restful.Response) {
 	usr := User{}
-	if err := request.ReadEntity(&usr); err != nil {
-		response.WriteError(http.StatusInternalServerError, err)
+	if err := req.ReadEntity(&usr); err != nil {
+		resp.WriteError(http.StatusInternalServerError, err)
 		return
 	}
 	u.users[usr.ID] = usr
-	response.WriteHeaderAndEntity(http.StatusCreated, usr)
+	resp.WriteHeaderAndEntity(http.StatusCreated, usr)
 }
 
-func (u *UserResource) removeUser(request *restful.Request, response *restful.Response) {
-	id, err := strconv.Atoi(request.PathParameter("userID"))
+func (u *UserResource) removeUser(req *restful.Request, resp *restful.Response) {
+	id, err := strconv.Atoi(req.GetParameter(u.ppUID))
 	if err != nil {
-		response.WriteErrorString(http.StatusNotFound, "User could not be found.")
+		resp.WriteErrorString(http.StatusBadRequest, "User ID is invalid.")
 		return
 	}
 	delete(u.users, id)
+	resp.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
-	u := UserResource{map[int]User{}}
+	auth := &Auth{
+		secret:          "secret",
+		hpAuthorization: restful.HeaderParameter("authorization", "JWT in authorization header"),
+	}
+	restful.DefaultContainer.Add(auth.WebService())
+
+	u := UserResource{
+		users: map[int]User{},
+		ppUID: restful.PathParameter("userID", "identifier of the user").DataType("int").Regex("\\d+").DefaultValue("1"),
+		auth:  auth,
+	}
 	restful.DefaultContainer.Add(u.WebService())
 
 	swaggerJson := "/apidocs.json"
@@ -212,7 +251,7 @@ func main() {
 	restful.DefaultContainer.Add(restfulspec.NewOpenAPIService(config))
 
 	basePath := "/apidocs/"
-	http.Handle(basePath, http.StripPrefix(basePath, http.FileServer(http.Dir("./vendor/github.com/swagger-api/swagger-ui/dist"))))
+	http.Handle(basePath, http.StripPrefix(basePath, http.FileServer(http.Dir("./swagger-ui/dist"))))
 
 	// Optionally, you may need to enable CORS for the UI to work.
 	cors := restful.CrossOriginResourceSharing{
@@ -246,7 +285,18 @@ func enrichSwaggerObject(swo *spec.Swagger) {
 			Version: "1.0.0",
 		},
 	}
-	swo.Tags = []spec.Tag{spec.Tag{TagProps: spec.TagProps{
-		Name:        "users",
-		Description: "Managing users"}}}
+	swo.Tags = []spec.Tag{
+		spec.Tag{
+			TagProps: spec.TagProps{
+				Name:        "authentication",
+				Description: "Authentication",
+			},
+		},
+		spec.Tag{
+			TagProps: spec.TagProps{
+				Name:        "users",
+				Description: "Managing users",
+			},
+		},
+	}
 }
